414bd548f844e0ecf1b285d87346b8ad0d26667b
Toshihiko Chen <toshihikochen@qq.com>
2022-10-10 17:36:36 +0800

    Remove SSIM loss, change depth loss back to l1 loss.

--- losses/depth_loss.py
+++ losses/depth_loss.py
@@ -4,10 +4,19 @@ import torch
 import torch.nn as nn
 
 
+# class DepthLoss(nn.Module):
+#     def __init__(self, alpha=1.0):
+#         super(DepthLoss, self).__init__()
+#         self.register_buffer("alpha", torch.tensor(alpha))
+#
+#     def forward(self, y_pred, y_true):
+#         return torch.mean(torch.log(torch.abs(y_pred - y_true) + self.alpha))
+
+
 class DepthLoss(nn.Module):
-    def __init__(self, alpha=1.0):
+    def __init__(self):
         super(DepthLoss, self).__init__()
-        self.register_buffer("alpha", torch.tensor(alpha))
+        self.l1loss = nn.L1Loss()
 
     def forward(self, y_pred, y_true):
-        return torch.mean(torch.log(torch.abs(y_pred - y_true) + self.alpha))
+        return self.l1loss(y_pred, y_true)
